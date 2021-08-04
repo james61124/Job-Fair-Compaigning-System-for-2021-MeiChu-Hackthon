0e548c59f2dae32439adca948f073759c4fa8153
James <james61124@gmail.com>
2021-08-04 15:13:42 +0800

    'ok'

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ def handle_message(event):
         #line_bot_api.reply_message(event.reply_token, message)
     #else:
         message = TextSendMessage(text=msg)
-        line_bot_api.push_message(to, TextSendMessage(text='Hello World!'))
+        #line_bot_api.push_message(to, TextSendMessage(text='Hello World!'))
         line_bot_api.reply_message(event.reply_token, message)
-    
+        line_bot_api.reply_message(event.reply_token, message)
     
 
 import os
