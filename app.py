e21d96599ff1ef0b61ed2f5f584265a28410cf45
James <james61124@gmail.com>
2021-08-04 11:26:49 +0800

    ok

--- app.py
+++ app.py
@@ -45,31 +45,36 @@ def callback():
     return 'OK'
 
 
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = "喔喔"+event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-    else:
+    if '123' in MessageEvent:
+        msg = "這是第一關"+event.message.text
+    #if '最新合作廠商' in msg:
+       # message = imagemap_message()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #elif '最新活動訊息' in msg:
+        #message = buttons_message()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #elif '註冊會員' in msg:
+       # message = Confirm_Template()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #elif '旋轉木馬' in msg:
+        #message = Carousel_Template()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #elif '圖片畫廊' in msg:
+        #message = test()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #elif '功能列表' in msg:
+       # message = function_list()
+        #line_bot_api.reply_message(event.reply_token, message)
+    #else:
+        message = TextSendMessage(text=msg)
         message = TextSendMessage(text=msg)
         line_bot_api.reply_message(event.reply_token, message)
+    
+    
 
 import os
 if __name__ == "__main__":
